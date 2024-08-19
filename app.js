c9def60b609c30b7494af594835e348e3d49f772
AImadeMeMakeThisAccount <136371236+AImadeMeMakeThisAccount@users.noreply.github.com>
2024-08-18 20:39:12 -0700

    Adding more to app.js

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 /**
- * Landing Page 
+ * Landing Phase 
  *  A way to create a game 
  *  A way to join an existing gaming
  *  A required name field?
 /
 
+/**
+ * Lobby Phase
+ * (Room code in lobby only)
+ * A way to start after minimum number of players have joined room 
+ * A limit to how many players can join
+ * A way to know the number of players in a room 
+ */
+
+/**
+ * Game Phase 
+ */
